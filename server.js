1989f712bb7d1439062c2ac05135d313d57d023e
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-05-01 09:15:33 -0600

    adds GET endpoint for all states

--- server.js
+++ server.js
@@ -6,4 +6,17 @@ const database = require('knex')(configuration);
 const app = express()
 const port = 3000
 
+app.get('/', (req, res) => console.log('Hello World!'))
+
+app.get('/api/v1/states', (req, res) => {
+  database('states').select()
+    .then((states) => {
+      res.status(200).json(states)
+    })
+    .catch(error => res.status(500).json({ error }))
+})
+
+
+
+
 app.listen(port, () => console.log(`App is listening on port ${port}`))
