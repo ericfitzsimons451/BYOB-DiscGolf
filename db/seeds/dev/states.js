d20c64cec1b9211e9c2a855b039050dc5a9117a1
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-05-03 09:54:15 -0600

    adds

--- db/seeds/dev/states.js
+++ db/seeds/dev/states.js
@@ -14,10 +14,6 @@ const createState = (knex, state) => {
           name: course.name,
           state_id: stateIds[0],
           city: course.city,
-<<<<<<< HEAD
-          state: course.state,
-=======
->>>>>>> 5eb685539ccf59863db71aabf47fa399efb4e2e2
           holes: course.holes,
           multiplePins: course.multiplePins,
           majorTournament: course.majorTournament,
